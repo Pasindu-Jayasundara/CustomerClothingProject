ad80a165dd71b044ebb484fbd437a62bf0626faf
Sanka Udeshika <147336157+SankaUdeshika@users.noreply.github.com>
2024-10-22 17:57:26 +0530

    Designed Shape Body page

--- js/details.js
+++ js/details.js
@@ -5,10 +5,36 @@ function changeRangeAge() {
 
 function changeRangeWeight() {
   var ageRange = document.getElementById("WeightRange").value;
-  document.getElementById("WeightOutput").innerText = ageRange+" lbs";
+  document.getElementById("WeightOutput").innerText = ageRange + " lbs";
 }
 
 function changeRangeheight() {
-    var ageRange = document.getElementById("heightRange").value;
-    document.getElementById("HeightOutput").innerText = ageRange+" cm";
-  }
+  var ageRange = document.getElementById("heightRange").value;
+  document.getElementById("HeightOutput").innerText = ageRange + " cm";
+}
+
+function Next2Page() {
+  window.location = "ShapeBody.html";
+}
+
+function next3Page() {
+  window.location = "shoulderType.html";
+}
+
+function chooseBodyShape(shape) {
+  document.getElementById("Triangle").classList.remove("active");
+  document.getElementById("rectangle").classList.remove("active");
+  document.getElementById("inverted").classList.remove("active");
+  document.getElementById("dontknow").classList.remove("active");
+
+  document.getElementById(shape).classList.toggle("active");
+}
+
+function chooseShoulderType(shape) {
+  document.getElementById("straight").classList.remove("active");
+  document.getElementById("normal").classList.remove("active");
+  document.getElementById("sloping").classList.remove("active");
+  document.getElementById("dontknow").classList.remove("active");
+
+  document.getElementById(shape).classList.toggle("active");
+}
